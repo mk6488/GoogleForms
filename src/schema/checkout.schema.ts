509477ada965d162a291cbd93a13b58ace9930d7
Mike Katholnig <mk3819@gmail.com>
2023-06-25 14:12:07 +0100

    delivery info form management

--- src/schema/checkout.schema.ts
+++ src/schema/checkout.schema.ts
@@ -5,4 +5,15 @@ export const PersonalInfoSchema = z.object({
   email: z.string().email({ message: "Please provide a valid email" }),
 });
 
+export const DeliveryInfoSchema = z.object({
+  city: z.string(),
+  postCode: z.string(),
+  address: z.string(),
+  shipping: z.enum(["free", "fast", "same_day"]),
+});
+
+export const PaymenyInfoSchema = z.object({});
+
 export type PersonalInfo = z.infer<typeof PersonalInfoSchema>;
+export type DeliveryInfo = z.infer<typeof DeliveryInfoSchema>;
+export type PaymentInfo = z.infer<typeof PaymenyInfoSchema>;
